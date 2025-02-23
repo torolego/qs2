68bee70f769c68786c51bbf4a06423325b7efc94
torolego <mattmau@gmail.com>
2025-02-23 19:50:28 +0100

    Update __manifest__.py
    license

--- custom_sale_rental_alert/__manifest__.py
+++ custom_sale_rental_alert/__manifest__.py
@@ -2,8 +2,9 @@
     'name': 'Sale Rental Alert',
     'version': '17.0.1.0.0',
     'summary': 'Avviso di sovrapposizione prenotazioni noleggio',
-    'author': 'QuickStart2',
+    'author': 'Quickstart2',
     'category': 'Sales',
+    'license': 'LGPL-3',
     'depends': ['sale', 'sale_renting'],
     'data': [
         'views/sale_renting_views.xml',
